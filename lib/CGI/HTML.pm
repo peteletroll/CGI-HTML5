771811ad10682dc2d09a72939c82ac420a959fb0
Pietro Cagnoni <pietro.cagnoni@gmail.com>
2020-10-04 16:43:12 +0200

    tag list updated

--- lib/CGI/HTML.pm
+++ lib/CGI/HTML.pm
@@ -26,27 +26,29 @@ sub clone($) {
 
 ### initialization
 
+# from https://developer.mozilla.org/en-US/docs/Web/HTML/Element
 my @TAG = qw(
-	a abbr acronym address area
-	b base bdo big blockquote body br button
-	caption cite code col colgroup
-	dd del dfn div dl dt
-	em
-	fieldset form frame frameset
-	h1 h2 h3 h4 h5 h6
-	head hr html
+	a abbr address area article aside audio
+	b base bdi bdo blockquote body br button
+	canvas caption cite code col colgroup
+	data datalist dd del details dfn dialog div dl dt
+	em embed
+	fieldset figcaption figure footer form
+	h1 h2 h3 h4 h5 h6 head header hgroup hr html
 	i iframe img input ins
 	kbd
 	label legend li link
-	map meta
-	noframes noscript
-	object ol optgroup option
-	p param pre
+	main map mark menu meta meter
+	nav noscript
+	object ol optgroup option output
+	p param picture pre progress
 	q
-	samp script select small span strong style sub sup
-	table tbody td textarea tfoot th thead title tr tt
-	ul
-	var
+	rb rp rt rtc ruby
+	s samp script section select slot small source span strong style sub summary sup
+	table tbody td template textarea tfoot th thead time title tr track
+	u ul
+	var video
+	wbr
 );
 
 # from https://developer.mozilla.org/en-US/docs/Glossary/empty_element
